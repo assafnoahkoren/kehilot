beeff3208dc541f9198d42e73b228cf742f72601
Assaf Koren <assafnoahkoren@gmail.com>
2024-03-16 22:55:18 +0200

    reset hasura

---
+++ projects/db/prisma/migrations/20240316204857_init/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "Profile" (
+    "id" TEXT NOT NULL DEFAULT gen_random_uuid(),
+    "email" TEXT NOT NULL,
+    "picture_url" TEXT,
+    "first_name" TEXT,
+    "last_name" TEXT,
+    "phone" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+
+    CONSTRAINT "Profile_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Profile_email_key" ON "Profile"("email");
