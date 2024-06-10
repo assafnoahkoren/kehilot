1744fb7cfe9be110d8bcb14d6f9df021d01eb169
Assaf Noah Koren <assaf.koren@connecteam.com>
2024-06-10 09:32:05 +0300

    added poll card

---
+++ projects/client/src/view/components/poll-card/poll-card.stories.tsx
@@ -0,0 +1,51 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { CallCard } from './call-card';
+import { Button } from '@mui/material';
+import { PollCard } from './poll-card.tsx';
+import { Tag } from '../tag/tag.tsx';
+
+
+const meta = {
+  title: 'Components/PollCard',
+  component: PollCard,
+  argTypes: {
+  },
+
+} satisfies Meta<typeof PollCard>;
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+
+export const Default: Story = {
+  args: {
+    title: '5 תושבים',
+    subtitle: 'שינו את מיקומם מהשבוע שעבר',
+    upperButton: <Button variant="text">
+      לצפייה
+      <i className="fas fa-chevron-right ms-2"></i>
+    </Button>,
+    icon: <i className="fas fa-location-pin text-3xl p-3 text-primary"></i>
+  },
+  argTypes: {
+
+  },
+};
+
+export const WithTags: Story = {
+  args: {
+    title: '3 פניות חדשות',
+    subtitle: <div className="flex gap-2">
+      בנושאים:
+      <Tag type={'chip'} label={'מזון'}></Tag>
+      <Tag type={'chip'} label={'חינוך'}></Tag>
+      </div>,
+    upperButton: <Button variant="text">
+      לצפייה
+      <i className="fas fa-chevron-right ms-2"></i>
+    </Button>,
+    icon: <i className="fas fa-user text-2xl p-4 px-8 text-primary"></i>
+  },
+  argTypes: {
+
+  },
+};
---
+++ projects/client/src/view/components/poll-card/poll-card.tsx
@@ -0,0 +1,40 @@
+import { Button, Chip, ChipProps, Paper } from "@mui/material";
+import { FC, ReactNode, useState } from "react";
+import { Tag, TagProps } from "../tag/tag";
+
+
+interface PollCardProps {
+  title: ReactNode;
+  subtitle?: ReactNode;
+  upperButton?: ReactNode;
+  icon?: ReactNode;
+}
+
+export const PollCard: FC<PollCardProps> = props => {
+	const date = new Date(props.date);
+	return (
+    <div className="min-w-[328px] rounded-lg shadow flex items-center justify-between" style={{color: '#0A2342'}}>
+      <div>
+        {props.icon}
+      </div>
+      <div className="flex flex-col flex-1">
+        <div className="p-3 ps-0">
+          <div className="flex items-center justify-between gap-2">
+            <div className="flex gap-2 items-cente font-bold text-lg">
+              <div>
+                {props.title}
+              </div>
+            </div>
+            <div>
+              {props.upperButton}
+            </div>
+          </div>
+          <div className="font-bold">
+            {props.subtitle}
+          </div>
+
+        </div>
+      </div>
+    </div>
+	);
+}
