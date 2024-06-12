e9b1f7c2d2258b42f3b0882d9b18106402c2c2b1
Assaf Koren <assafnoahkoren@gmail.com>
2024-06-12 21:30:36 +0300

    added hasura scripts

---
+++ projects/hasura/scripts/export.sh
@@ -0,0 +1,1 @@
+hasura metadata export --admin-secret "$HASURA_GRAPHQL_ADMIN_SECRET" --endpoint "$HASURA_ENDPOINT" # --log-level DEBUG
--- projects/hasura/scripts/zeus.sh
+++
@@ -1,1 +0,0 @@
-hasura metadata export --admin-secret "$HASURA_GRAPHQL_ADMIN_SECRET" --endpoint "$HASURA_ENDPOINT" # --log-level DEBUG
