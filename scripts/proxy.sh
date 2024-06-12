8e1ca11615900ff6273702f200beb69f26d4728a
Assaf Koren <assafnoahkoren@gmail.com>
2024-06-12 20:30:11 +0300

    cleaning

--- scripts/proxy.sh
+++
@@ -1,4 +0,0 @@
-echo "Port-forwarding into the cluster..."
-export PG_POD=$(kubectl get pods -n $NAMESPACE -l app=postgres -o custom-columns=:metadata.name --no-headers) # Get the name of the postgres pod
-echo "Postgres pod: $PG_POD" 
-kubectl port-forward pod/$PG_POD 5432:5432 -n $NAMESPACE
